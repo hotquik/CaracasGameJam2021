c0ae8348997d11f2ced39cefdb006cf71517ac1d
dcavadia <dan.acppp@gmail.com>
2021-01-28 19:33:34 -0400

    temporary movement controller

--- Unity/Assets/Scripts/Player/PlayerController.cs
+++ Unity/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-        
-    }
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.transform.position += Movement * 3 * Time.deltaTime;
     }
 }
